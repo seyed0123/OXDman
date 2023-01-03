6eb9669f6145227038a278721cee721329bf9403
seyed <seyed@seyed.com>
2023-01-03 20:27:29 +0330

    a basic menu created for later

---
+++ source1/#base.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void showMenu();
+void printpacman();
+void ranks();
+
+int main()
+{
+
+	return 0;
+}
+void ranks()
+{
+	system("cls");
+	string line;
+	ifstream file("ranks.pacman");
+	if(file.is_open())
+	{
+
+		return ;
+	}else
+	{
+		cout<<"sorry something went wrong so your load failed :(((\n";
+		return ;
+	}
+}
+void printpacman()
+{
+	cout<<"       *********           **              *********             **            **                    **              **      **\n";
+	cout<<"       **     **         **  **            **                  **  **        **  **                **  **            ****    **\n";
+	cout<<"       **     **        **    **           **                 **    **      **     **             **    **           ** **   **\n";
+	cout<<"       **     **       **********          **                **      **    **       **           **********          **  **  **\n";
+	cout<<"       *********      **        **         **               **        **  **         **         **        **         **   ** **\n";
+	cout<<"       **            **          **        **              **          ****          **        **          **        **    ****\n";
+	cout<<"       **           **            **       **********     **            **            **      **            **       **      **\n";
+
+}
+void showMenu()
+{
+	printpacman();
+	cout<<" 1- New game\n 2- Load a game:\n 3- Ranking\n 4- Exit\n";
+	string input;
+	cin>>input;
+	if(input=='1')
+	{
+		return;
+	}else if(input =='2')
+	{
+		load();
+		return;
+	}else if(input=='3')
+	{
+		ranks();
+		showMenu();
+	}else if(input == '4')
+	{
+		exit(0);
+	}else
+	{
+		system("cls");
+		cout<<"your input isn't valid.\n";
+		showMenu();
+	}
+}
